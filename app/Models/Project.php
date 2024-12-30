8403e024a9b76c71b9162e15301e3e898c8234df
zakius <2071503+zakius@users.noreply.github.com>
2024-12-30 13:06:05 +0100

    add filament, models and migrations

---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date'
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime'
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'user_id',
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+        'status'
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
